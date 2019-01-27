19237cd519a6a7898e971a0c9f4f154081127501
iRED <foodrickme@gmail.com>
2019-01-26 20:14:53 -0800

    ClientOptions#fetch default true -> false

--- src/structures/options.js
+++ src/structures/options.js
@@ -1,11 +1,11 @@
 /**
  * @typedef {object} ClientOptions
- * @property {boolean} [fetch=true] When set to true, will fetch a Bot before emitting an event. Requires `simple.space` be installed.
+ * @property {boolean} [fetch=false] When set to true, will fetch a Bot before emitting an event. Requires `simple.space` be installed.
  * @property {boolean} [raw=false] When set to true, returns the raw object of the bot. Requires {@link ClientOptions#fetch} be true.
  * @property {string[]} [tokens=[]] An array of bot tokens provided from botlist.space to receive the bot's events from.
  */
 exports.ClientOpts = {
-	fetch: true,
+	fetch: false,
 	raw: false,
 	tokens: [],
 };
